dc913baf14370d2c341ef28519f5c751f284f549
KateMaruk <marukkatia@gmail.com>
2018-11-17 16:54:13 +0200

    first commit

---
+++ src/main/java/MarukExamProject/App.java
@@ -0,0 +1,13 @@
+package MarukExamProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
